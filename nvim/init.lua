f30345217bae5f926dfa6bc0c64e143e1308b99f
Ollie Charles <ollie@ocharles.org.uk>
2022-06-19 18:18:26 +0100

    Use hop.nvim for fF and tT

--- nvim/init.lua
+++ nvim/init.lua
@@ -237,4 +237,12 @@ vim.api.nvim_set_keymap('n', '<Space>hl', "<cmd>:HopLineStart<cr>", {})
 -- tidy
 require('tidy').setup()
 
+-- Replace normal f/F and t/T movements
+vim.api.nvim_set_keymap('n', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>",                         {})
+vim.api.nvim_set_keymap('n', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>",                        {})
+vim.api.nvim_set_keymap('o', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, inclusive_jump = true })<cr>",  {})
+vim.api.nvim_set_keymap('o', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, inclusive_jump = true })<cr>", {})
+vim.api.nvim_set_keymap('',  't', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>",                         {})
+vim.api.nvim_set_keymap('',  'T', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>",                        {})
+
 vim.cmd[[colorscheme catppuccin]]
